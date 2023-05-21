15a466892645d7ca6dbc902d282eef37d2da786c
zhuolin <zhuolin1014@163.com>
2023-05-21 20:55:30 +0800

    模块化Money

---
+++ src/components/Money/Tags.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="tags">
+    <ul class="current">
+      <li>
+        <Icon name="other" />
+        <span>其他</span>
+      </li>
+      <li>
+        <Icon name="catering" />
+        <span>餐饮</span>
+      </li>
+      <li>
+        <Icon name="traffic" />
+        <span>交通</span>
+      </li>
+      <li>
+        <Icon name="shopping" />
+        <span>购物</span>
+      </li>
+      <li>
+        <Icon name="reside" />
+        <span>居住</span>
+      </li>
+      <li>
+        <Icon name="amusement" />
+        <span>娱乐</span>
+      </li>
+      <li>
+        <Icon name="medical" />
+        <span>医疗</span>
+      </li>
+    </ul>
+    <div class="new">
+      <button>新增标签</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.tags {
+  font-size: 14px;
+  padding: 16px;
+  flex-grow: 1;
+  > .current {
+    display: flex;
+    flex-wrap: wrap;
+    > li {
+      display: flex;
+      flex-direction: column;
+      width: 20%;
+      padding: 16px;
+      flex-wrap: wrap;
+      justify-content: center;
+      align-content: center;
+      .icon {
+        width: 28px;
+        height: 28px;
+      }
+      > span {
+        text-align: center;
+      }
+    }
+  }
+  > .new {
+    padding-left: 16px;
+    button {
+      background: transparent;
+      border: none;
+      color: #999;
+      border-bottom: 1px solid;
+      padding: 0 4px;
+    }
+  }
+}
+</style>
---
+++ src/components/Money/Types.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <ul class="types">
+      <li class="selected">支出</li>
+      <li>收入</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.types {
+  background: #e8e8e8;
+  font-size: 20px;
+  display: flex;
+  justify-content: center;
+  > li {
+    margin: 0 16px;
+    height: 44px;
+    display: flex;
+    flex-wrap: wrap;
+    align-content: center;
+    position: relative;
+    &.selected::after {
+      content: "";
+      position: absolute;
+      bottom: 0;
+      left: 0;
+      width: 100%;
+      height: 2px;
+      background: #333;
+    }
+  }
+}
+</style>
