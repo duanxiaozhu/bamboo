e57962dac222c3e78c8c3e6674ff3c13b6ae8255
zhuolin <zhuolin1014@163.com>
2023-06-08 02:07:54 +0800

    2

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/bamboo/'
+    ? '/bamboo-website/'
     : '/',
   lintOnSave: false,
   chainWebpack: config => {
